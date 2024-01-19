a96c9dc517e87c8854e32e3f42a228b10d3d0123
dhkcos16 <timeseller9@naver.com>
2024-01-19 15:07:57 +0900

    history works

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Board from "./components/Board";
 
 const App = () => {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
   const calculateWinner = (squares) => {
@@ -30,7 +31,7 @@ const App = () => {
     return null;
   };
 
-  const current = history[history.length - 1]; // 히스토리 배열의 마지막을 잡아줘야 하는데, 3번째가 마지막이면 lenght:3 이고 index는 2가 되므로 -1을 해준다.
+  const current = history[stepNumber]; // 히스토리 배열의 마지막을 잡아줘야 하는데, 3번째가 마지막이면 lenght:3 이고 index는 2가 되므로 -1을 해준다.
   const winner = calculateWinner(current.squares); // 히스토리 배열의 마지막 배열의 squares를 불러옴.
 
   let status;
@@ -41,17 +42,36 @@ const App = () => {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice();
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
     if (calculateWinner(newSquares) || newSquares[i]) {
       // 누군가 승리하거나 square가 이미 클릭 되어졌다면, 클릭을 무시하도록 변경 ! 결과가 나오면 로직을 끊어주는 코드.
       return;
     }
 
     newSquares[i] = xIsNext ? "X" : "O"; // 조건에 따라 X or O
-    setHistory([...history, { squares: newSquares }]); // 기존:newSquares => 변경:  history 함수를 펼침. 추가된 squares를 history에 추가한다.
-    setXIsNext((current) => !current); // 함수형. 여러줄 복제해서 쓰더라도 다 적용됨 good!
+    setHistory([...newHistory, { squares: newSquares }]); // 기존:newSquares => 변경:  history 함수를 펼침. 추가된 squares를 history에 추가한다.
+    setXIsNext((prev) => !prev); // 함수형. 여러줄 복제해서 쓰더라도 다 적용됨 good!
     // setXIsNext(!xIsNext); // 간단하지만 여러줄 복제해서 써도 한 번만 적용됨. bad
+    setStepNumber(newHistory.length);
   };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -61,6 +81,7 @@ const App = () => {
       <div className="game-info">
         <div className="status">{status}</div>
       </div>
+      <ol>{moves}</ol>
     </div>
   );
 };
